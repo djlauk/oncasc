b993abf91a5d6586c0aba05eb4a9c6d1c7ffb22f
Daniel J. Lauk <daniel.lauk@gmail.com>
2016-09-28 08:58:37 +0200

    Examples and improvements to the scheduling.
    Scheduling improvements in detail:
    
    - Allow time slots not to be scheduled by providing a comment
      in the time slot definition file.
    
    - Prevent same team member from being assigned twice in a row.
    
    - Make shuffling of the team an option, not the default.

--- README.md
+++ README.md
@@ -5,3 +5,65 @@ duties in a fair fashion.
 
 To achieve that, *oncasc* utilizes a round-robin scheme, but additionally
 it takes team members' absences into account as well.
+
+
+## Usage example
+
+This is the command you need to run to process the examples provided along with *oncasc*:
+
+    $ python src/oncasc.py -t examples/example_team -s examples/example_times
+
+And this will be your output:
+
+    01: # Week 1 needs no scheduling: Christmas / new year break
+    02: Bob
+    03: Claire
+    04: Alice
+    05: Bob
+    06: Dave
+    07: Alice
+    08: Claire
+    09: Dave
+    10: Alice
+    11: Bob
+    12: Claire
+    13: Dave
+    14: Alice
+    15: Claire
+    16: Dave
+    17: Bob
+    18: Alice
+    19: Bob
+    20: # Big maintenance break, 3 weeks of restructuring, hence no on call service
+    21: # Big maintenance break, 3 weeks of restructuring, hence no on call service
+    22: # Big maintenance break, 3 weeks of restructuring, hence no on call service
+    23: Claire
+    24: Dave
+    25: Alice
+    26: Bob
+    27: Claire
+    28: Dave
+    29: Alice
+    30: Bob
+    31: Claire
+    32: Dave
+    33: Alice
+    34: Bob
+    35: Claire
+    36: Dave
+    37: Alice
+    38: Bob
+    39: Claire
+    40: Dave
+    41: Alice
+    42: Bob
+    43: Claire
+    44: Dave
+    45: Alice
+    46: Bob
+    47: Claire
+    48: Dave
+    49: Alice
+    50: Bob
+    51: Claire
+    52: # Week 52 needs no scheduling: Christmas / new year break
--- src/oncasc.py
+++ src/oncasc.py
@@ -10,12 +10,12 @@ import json
 import os
 import random
 import sys
-import time
 
 
 __PROG = os.path.basename(sys.argv[0])
 __DEBUG_ENABLED = os.environ.get('DEBUG', '0').lower() in ('1', 'yes', 'y', 'on', 'true')
 if __DEBUG_ENABLED:
+    import time
     import traceback
     def debug(msg):
         sys.stdout.write("DEBUG: %s %s\n" % (time.strftime('%Y-%m-%d %H:%M:%S'), msg))
@@ -36,21 +36,27 @@ class TeamMember(object):
 
 def main():
     debug("Starting")
-    opts, args = getopt.getopt(sys.argv[1:], 'ht:s:')
+    opts, args = getopt.getopt(sys.argv[1:], 'hrs:t:')
     team = None
     time_slots = None
+    randomize = None
     for o, v in opts:
         if o == '-h':
             help()
             sys.exit(0)
-        elif o == '-t':
-            team = read_team(v)
+        elif o == '-r':
+            randomize = True
         elif o == '-s':
             time_slots = read_time_slots(v)
+        elif o == '-t':
+            team = read_team(v)
     if not team:
         raise RuntimeError("No team file provided")
     if not time_slots:
         raise RuntimeError("No time slots provided")
+    if randomize:
+        random.shuffle(team)
+        debug("Team after shuffling:  %s" % [str(x) for x in team])
     schedule = generate_schedule(team, time_slots)
     print_schedule(schedule)
     debug("Done")
@@ -62,11 +68,12 @@ Usage: %(prog)s OPTIONS
 
 OPTIONS
   -h          Display instructions and exit
+  -r          Randomize ("shuffle") the team
+  -s FILE     Read times for schedule from FILE
   -t FILE     Read on call team info from FILE
-  -s FILE     Read schedule from FILE
 """ % {
     'prog': os.path.basename(sys.argv[0])
-})
+  })
 
 
 def read_team(fname):
@@ -91,44 +98,62 @@ def read_file(fname):
 
 def read_time_slots(fname):
     debug("Reading time slots from %s" % fname)
-    return read_file(fname)
+    time_slots = {}
+    for li in read_file(fname):
+        parts = [x.strip() for x in li.split(':', 1)]
+        if len(parts) == 1:
+            time_slots[parts[0]] = None
+        else:
+            time_slots[parts[0]] = parts[1] or None  # empty string, if somebody puts in a colon, but no reason
+    return time_slots
 
 
 def generate_schedule(team, time_slots):
     schedule = {}
-    random.shuffle(team)
-    debug("Shuffled team: %s" % str([str(x) for x in team]))
     skipped = []
     next_team_member = 0
-    for slot in time_slots:
-        debug("Processing time slot '%s'" % str(slot))
+    last_assigned = None
+    for slot in sorted(time_slots.keys()):
+        debug("Processing time slot '%s'" % slot)
+        if time_slots[slot]:
+            debug("Needs no schedule: %s" % time_slots[slot])
+            schedule[slot] = time_slots[slot]
+            continue
         if len(skipped) > 0:
             debug("Checking skipped people...")
             found = False
             for i in range(len(skipped)):
                 debug("Checking skipped person %d: %s" % (i, skipped[i]))
-                if slot not in skipped[i].unavailable:
+                if slot in skipped[i].unavailable:
+                    debug("Person is not available")
+                elif skipped[i] is last_assigned:
+                    debug("Person already did time slot before...")
+                else:
                     debug("Person is available")
                     schedule[slot] = skipped[i].name
+                    last_assigned = skipped[i]
                     del skipped[i]
                     found = True
                     break
-                else:
-                    debug("Person is not available")
             if found:
                 continue   # proceed to next slot, if this one could be assigned
+            debug("Skipped people exhausted, continue to regular team roster")
 
         debug("Checking team, starting at index %d" % next_team_member)
         found = False
         for offset in range(len(team)):
             i = (next_team_member + offset) % len(team)
-            debug("Check %d: %s" % (i, team[i]))
+            debug("Check team member %d: %s" % (i, team[i]))
             if slot in team[i].unavailable:
                 debug("Person is not available... postponing")
+                skipped.append(team[i])
+            elif team[i] is last_assigned:
+                debug("Person already did time slot before... postponing")
                 skipped.append(team[i])
             else:
                 debug("Person is available")
                 schedule[slot] = team[i].name
+                last_assigned = team[i]
                 next_team_member = (i + 1) % len(team)
                 found = True
                 break
@@ -140,9 +165,7 @@ def generate_schedule(team, time_slots):
 
 
 def print_schedule(schedule):
-    keys = schedule.keys()
-    keys.sort()
-    for k in keys:
+    for k in sorted(schedule.keys()):
         sys.stdout.write("%s: %s\n" % (k, schedule[k]))
 
 
